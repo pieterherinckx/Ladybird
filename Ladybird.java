2911a03ddab70f9c62a4467c099f3096725807b5
pieterherinckx <pieterherinckx@atheneumtienen.be>
2019-03-15 19:25:44 +0100

    Create Ladybird.java

---
+++ Ladybird.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
+
+/**
+ * This class defines a crab. Crabs live on the beach. They like sand worms 
+ * (very yummy, especially the green ones).
+ */
+public class Ladybird extends Animal
+{
+    public void act()
+    {
+        
+    }
+}
+
+
